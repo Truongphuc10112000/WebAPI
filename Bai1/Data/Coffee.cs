33468cd722929f41eaedecbb40f8978dde3eab5c
Truongphuc10112000 <Truongphucc102@gmail.com>
2023-02-09 22:23:59 +0700

    Add project files.

---
+++ Bai1/Data/Coffee.cs
@@ -0,0 +1,12 @@
+﻿namespace Bai1.Data
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string MaCoffee { get; set; }
+        public string TenCoffee { get; set; }
+        public string description { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+    }
+}
